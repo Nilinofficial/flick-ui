86c23b6b777fa9acd3f2d3b515bd38f1b24aae71
Nilin <itsmenilin18@gmail.com>
2024-10-19 21:48:37 +0530

    clear user from redux store

--- queries/useLogout.js
+++ queries/useLogout.js
@@ -1,21 +1,25 @@
 import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { removeUser } from "../slices/userSlice";
+import { useDispatch } from "react-redux";
 
-const logOutUser = async () => {
-  return await axios.post(
+const logOutUser = async (dispatch) => {
+  await axios.post(
     `${process.env.NEXT_PUBLIC_BASE_URL}/auth/logout`,
     {},
     {
       withCredentials: true,
     }
   );
+  await dispatch(removeUser());
 };
 
 export const useLogOut = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
   return useMutation({
-    mutationFn: logOutUser,
+    mutationFn: () => logOutUser(dispatch),
     onSuccess: () => {
       router.push("/login");
     },
